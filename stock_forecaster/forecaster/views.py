3cd60a389105df3f274c9eefa6ceac5432a4bea7
Isss11 <isaiah.sinclair.0711@gmail.com>
2024-05-05 20:18:49 -0400

    arima-forecaster: Implement Basic ARIMA API

--- stock_forecaster/forecaster/views.py
+++ stock_forecaster/forecaster/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import ARIMAForecaster
+from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
 
 # Constructs a ARIMA model to forecast stock returns, using ML
+@api_view(['POST'])
 def arimaForecast(request):
-    forecaster = ARIMAForecaster.ARIMAForecaster("C")
+    requestValues = JSONParser().parse(request)
+    forecaster = ARIMAForecaster.ARIMAForecaster(requestValues["ticker"])
     
-    # Obtains the actual approximated percentage returns from the training data, and the 15 days of forecasted returns
+    # Obtains the actual approximated percentage returns from the training data forecasted returns
     forecaster.createModel()
-    forecastedReturns = forecaster.getForecasts(15)
-    
+    forecastedReturns = forecaster.getForecasts(requestValues["forecastLength"])
     
     return HttpResponse(str(forecastedReturns))
