74a2b7deb706a5490e07ecb331eb5a81591e3d9c
IsaiahSinclairEcoTank <Isaiah@ecotankcanada.com>
2024-05-26 20:42:52 -0400

    Quantify Forecaster: Add LSTM Forecasting

--- stock_forecaster/forecaster/LSTMForecaster.py
+++ stock_forecaster/forecaster/LSTMForecaster.py
@@ -20,29 +20,32 @@ class LSTMForecaster:
         
     # Creates LSTM Model
     def createModel(self, lookBack):
-        normalizedPrices = self.stock.getNormalizedData(self.scaler)
+        self.lookBack = lookBack
+        
+        self.normalizedPrices = self.stock.getNormalizedData(self.scaler)
         
         # Create training and test data for model evaluation
         # Actual predictions (indicated in the app's UI) will be for data beyond current date
-        trainSize = int(len(normalizedPrices) * 0.7)
-        # testSize = len(normalizedPrices) - trainSize
+        trainSize = int(len(self.normalizedPrices) * 0.7)
+        # testSize = len(self.normalizedPrices) - trainSize
         
-        trainingData = normalizedPrices[0:trainSize,:]
-        testData = normalizedPrices[trainSize:len(normalizedPrices),:]
+        trainingData = self.normalizedPrices[0:trainSize,:]
+        testData = self.normalizedPrices[trainSize:len(self.normalizedPrices),:]
         
         # Create data-set with a given look-back amount
-        self.trainX, self.trainY = self.createDataset(trainingData, lookBack)
-        self.testX, self.testY = self.createDataset(testData, lookBack)
+        self.trainX, self.trainY = self.createDataset(trainingData, self.lookBack)
+        self.testX, self.testY = self.createDataset(testData, self.lookBack)
         
         # Reshape data to fit with model
         self.trainX = np.reshape(self.trainX, (self.trainX.shape[0], 1, self.trainX.shape[1]))
         self.testX = np.reshape(self.testX, (self.testX.shape[0], 1, self.testX.shape[1]))
         
-        self.model = self.getLSTM(lookBack, self.trainX, self.trainY, 3)
+        # TODO, make the number of Epocs a parameter
+        self.model = self.getLSTM(self.trainX, self.trainY, 1)
         
-    def getLSTM(self, lookBack, trainX, trainY, numEpochs):
+    def getLSTM(self, trainX, trainY, numEpochs):
         model = Sequential()
-        model.add(LSTM(4, input_shape=(1, lookBack)))
+        model.add(LSTM(4, input_shape=(1, self.lookBack)))
 
         # Adds a neural network layer with one input
         model.add(Dense(1))
@@ -65,8 +68,11 @@ class LSTMForecaster:
     
     # Analyzes quality of model, comparing the results of the training data and test data
     def measureModelAccuracy(self):
-        trainPredicted, trainActual = self.makeForecasts(self.trainX, self.trainY)
-        testPredicted, testActual = self.makeForecasts(self.testX, self.testY)
+        trainPredicted = self.makeForecasts(self.trainX)
+        testPredicted = self.makeForecasts(self.testX)
+        
+        trainActual = self.scaler.inverse_transform([self.trainY])
+        testActual = self.scaler.inverse_transform([self.testY])
         
         trainScore = self.computeError(trainPredicted, trainActual)
         testScore = self.computeError(testPredicted, testActual)
@@ -77,25 +83,53 @@ class LSTMForecaster:
         return np.sqrt(mean_squared_error(actual[0], predicted[:,0]))
     
     # Develops Forecasts
-    def makeForecasts(self, X, y):
+    def makeForecasts(self, X):
+        print("Making forecasts with X")
+        print(X.shape)
+        print(X)
         predictions = self.model.predict(X)
         
         # Change predictions back to original units to calculate RMSE in original units
-        predictionsTransformed = self.scaler.inverse_transform(predictions)
-        yTransformed = self.scaler.inverse_transform([y])
+        return self.scaler.inverse_transform(predictions)
         
-        return predictionsTransformed, yTransformed
-    
     # For returning the final predictions, along with their actual values
     def getCombinedPrices(self):
         pass
     
+    def predictIntoFuture(self, s):
+        predictionList = self.normalizedPrices[-self.lookBack:]
+
+        # For forecasted values only
+        predictedNormalizedPrices = []
+
+        # Using look-back amount to append to normalized dataset and using forecasted data to predict future forecasted values
+        # Note that this will cause larger errors, but is needed to perform real forecasts
+        for day in range(s):
+            # Merging together the predicted and actual prices to use in future forecasts, if there are some predictions made
+            if len(predictedNormalizedPrices) != 0:
+                npPredicted = np.reshape(np.array(predictedNormalizedPrices), (-1, 1))    
+                combinedPrices = np.concatenate((self.normalizedPrices, npPredicted))
+            else:
+                combinedPrices = self.normalizedPrices
+            
+            # Taking the past amount of values, specified by the lookback amount. This will be used in the model predictions.
+            X = np.reshape(combinedPrices[-self.lookBack:], (-1, 1, self.lookBack))
+            
+            # Obtaining predicted price and adding it to the predicted prices array to be used in future forecasts (depending on lookback amount)
+            predictedPrice = self.model.predict(X)[0][0]
+            
+            predictedNormalizedPrices.append(predictedPrice)
+            
+        predictedPrices = self.scaler.inverse_transform(np.reshape(predictedNormalizedPrices, (-1, 1)))
+            
+        return predictedPrices
         
 # Class manual testing code
 if __name__ == "__main__":
-    forecaster = LSTMForecaster("AAPL", "2010-01-01")
+    forecaster = LSTMForecaster("C", "2010-01-01")
     forecaster.createModel(8)
     print(forecaster.measureModelAccuracy())
-    # print(modelDetails)
-    # stockReturnsWithForecasts = forecaster.getCombinedReturns(5)
-    # print(stockReturnsWithForecasts)
+
+    # Forecast in the future (beyond the training and sample data
+    print("Obtaining predictions")
+    print(forecaster.predictIntoFuture(20))
